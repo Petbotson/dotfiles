81b4ee1c4c25f8e3da9ec641da143bcd1a25f807
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-04-16 17:56:12 +0200

    fix(home-manager): syntax typo fix

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.vincent = import "./home.nix";
-	};
+	}
       ];
     };
   };
