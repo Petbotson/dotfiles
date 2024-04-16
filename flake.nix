6d2c0b5f2a73f1d1f52c4417f516f8214b37f3ad
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-04-16 18:35:12 +0200

    fix(home-manager): add home-manager into outputs of flake.nix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = { 
       vs-nb = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
