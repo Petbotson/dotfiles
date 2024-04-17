c7064369f81633aed54d24995d71f0b887618070
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-04-17 11:49:29 +0200

    feat(home-manager): add zsh as terminal

--- home.nix
+++ home.nix
@@ -86,4 +86,18 @@
     };
   };
 
+  # zsh config using home-manager
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+    ll = "ls -l";
+    update = "sudo nixos-rebuild switch";
+    };
+  history.size = 10000;
+  history.path = "${config.xdg.dataHome}/zsh/history";
+  };
 }
