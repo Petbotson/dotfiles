2bce82e54300ad436c82a4ccde368313cd9864ac
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-04-22 10:37:53 +0200

    fix: add oh-my-zsh to zsh config

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -5,15 +5,14 @@
     enableAutosuggestions = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
-#    };
 
     shellAliases = {
       ll = "ls  -lah";
     };
 
-#    oh-my-zsh = {
-#      enable = true;
-#      plugins = [ "terraform" ];
-#    };
-   };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "terraform" ];
+    };
+  };
 }
