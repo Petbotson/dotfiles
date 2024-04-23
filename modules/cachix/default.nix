61f5e9295bf33fcadac88f2f7f6a762ddefa81f4
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-04-23 21:28:24 +0200

    feat: add nix-community cache server

--- modules/cachix/default.nix
+++ modules/cachix/default.nix
@@ -1,7 +1,12 @@
 { pkgs, lib, inputs, ...}: 
 {
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
   };
 }
