1ff353aa2975f91126773cacc8f14429e79ef703
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-04-26 17:35:42 +0200

    feat: move from vscodium to vscode

--- home/dev/vscode.nix
+++ home/dev/vscode.nix
@@ -2,11 +2,8 @@
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
-      #vscodevim.vim
-      #yzhang.markdown-all-in-one
     ];
    };
 }
