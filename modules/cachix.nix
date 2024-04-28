cc711e94973784aaf29378f173ed5b250644fd8e
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-04-28 19:42:08 +0200

    feat(modules): add nix-community cache public key

--- modules/cachix.nix
+++ modules/cachix.nix
@@ -7,6 +7,7 @@
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
