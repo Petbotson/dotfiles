15bc518b27b728075cc4ada20c96d5f9771a5b25
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-05-02 17:55:40 +0200

    chore: nix flake update

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -3,6 +3,21 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    settings = {
+      
+      # autostart
+      exec-once = [
+        "systemctl --user import-environment &"
+        "hash dbus-update-activation-environment 2>/dev/null &"
+        "dbus-update-activation-environment --systemd &"
+        "nm-applet &"
+        "wl-clip-persist --clipboard both"
+        "swaybg -m fill -i $(find ~/Pictures/wallpapers/ -maxdepth 1 -type f) &"
+        "sleep 1 && swaylock"
+        "hyprctl setcursor Nordzy-cursors 22 &"
+        "waybar &"
+        "mako &"
+      ];
     xwayland = {
       enable = true;
     };
