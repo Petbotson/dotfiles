2947041a44adca40c5e81765778027ff7c214f88
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-05-03 19:18:41 +0200

    fix(core): add wireguard.nix to modules import

--- modules/default.nix
+++ modules/default.nix
@@ -5,5 +5,6 @@
     ./programs.nix   # this is needed, because some programs can be configured in hm but some things need system level acces (e.g. zsh)
     ./tuxedo-rs.nix
     ./wayland.nix
+    ./wireguard.nix
   ];
 }
