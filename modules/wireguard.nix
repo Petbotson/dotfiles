0c95dc28589c634bd1614ae9277881a7a32dd1b5
Vincent Schwalbach <mail@vincent-schwalbach.de>
2024-05-08 14:49:44 +0200

    fix(core): wireguard dns server & cleanup

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -8,21 +8,14 @@
         "10.99.99.3/32"
       ];
       dns = [
-        "10.99.99.1"
+        "10.99.0.1"
       ];
-
-      # To match firewall allowedUDPPorts (without this wg uses random port numbers).
       listenPort = 51820;
-
-      # Path to the private key file.
       privateKeyFile = "/etc/ssl/privkey/home-vpn.key";
-
       peers = [{
         publicKey = "ap8zG1OQKfFNlI52Wu54aCqNvlhiQSzSDPcUw5LIzHQ=";
         allowedIPs = [ "10.99.0.0/20" ];
         endpoint = "${server_ip}:51820";
-        
-        # persistentKeepalive = 25;
       }];
     };
   };
