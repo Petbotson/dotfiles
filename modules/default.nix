714bcb25e0738d22834b1f238831a0d5459608ee
Petbotson <81176836+Petbotson@users.noreply.github.com>
2024-10-05 16:57:59 +0200

    feat:remove tailscaled temporary

--- modules/default.nix
+++ modules/default.nix
@@ -3,7 +3,7 @@
     ./core
     ./programs.nix   # this is needed, because some programs can be configured in hm but some things need system level acces (e.g. zsh)
     ./stylix.nix
-    ./tailscale.nix
+    # ./tailscale.nix
     ./tuxedo-rs.nix
     ./wayland.nix
     ./wireguard.nix
